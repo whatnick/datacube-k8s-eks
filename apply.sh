2cf4756bca026d45efe9dfb0eb37455d4b5347e0
Santosh Mohan <santosh.mohan@ga.gov.au>
2019-11-12 09:18:19 +1100

    Enable terraform logging

--- apply.sh
+++ apply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -xe
 #set -o xtrace
 # 
 # Patch EKS nodes
@@ -14,6 +14,9 @@ fi
 export WORKSPACE=$1
 export WORKSPACESPATH=$2
 
+# Enable Terraform logging
+export TF_LOG=TRACE
+
 if [[ "$3" = "clean" ]]; then
     CLEAN="true"
 fi
@@ -25,47 +28,47 @@ if [ ! -z "$CLEAN" ]; then
 fi
 terraform init -backend-config $WORKSPACESPATH/$WORKSPACE/backend.cfg
 terraform plan -out infra.plan -input=false -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars"
-terraform apply -auto-approve infra.plan
+#terraform apply -auto-approve infra.plan
 rm infra.plan
-
-# Configure local kubernetes config
-# aws eks --region $(terraform output region) update-kubeconfig --name $(terraform output cluster_name)
-
-# Set up aws-auth
-popd
-
-# build worker nodes
-pushd nodes
-if [ ! -z "$CLEAN" ]; then
-    echo "Cleaning"
-    rm -rf .terraform
-fi
-terraform init -backend-config $WORKSPACESPATH/$WORKSPACE/backend.cfg 
-
-terraform workspace new "$WORKSPACE-blue" || terraform workspace select "$WORKSPACE-blue"
-terraform plan -out workersblue.plan -input=false \
-    -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars" \
-    -var group_enabled=true \
-    -var node_group_name=blue
-terraform apply -auto-approve workersblue.plan
-rm workersblue.plan
-
-terraform workspace new "$WORKSPACE-green" || terraform workspace select "$WORKSPACE-green"
-terraform plan -out workersgreen.plan -input=false \
-    -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars" \
-    -var group_enabled=false \
-    -var node_group_name=green
-terraform apply -auto-approve workersgreen.plan
-rm workersgreen.plan
-popd
-
-pushd addons
-if [ ! -z "$CLEAN" ]; then
-    rm -rf .terraform
-fi
-terraform init -backend-config $WORKSPACESPATH/$WORKSPACE/backend.cfg
-terraform workspace new "$WORKSPACE-addons" || terraform workspace select "$WORKSPACE-addons"
-terraform plan -out addons.plan -input=false -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars"
-terraform apply -auto-approve addons.plan
-rm addons.plan
-popd
+#
+## Configure local kubernetes config
+## aws eks --region $(terraform output region) update-kubeconfig --name $(terraform output cluster_name)
+#
+## Set up aws-auth
+#popd
+#
+## build worker nodes
+#pushd nodes
+#if [ ! -z "$CLEAN" ]; then
+#    echo "Cleaning"
+#    rm -rf .terraform
+#fi
+#terraform init -backend-config $WORKSPACESPATH/$WORKSPACE/backend.cfg
+#
+#terraform workspace new "$WORKSPACE-blue" || terraform workspace select "$WORKSPACE-blue"
+#terraform plan -out workersblue.plan -input=false \
+#    -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars" \
+#    -var group_enabled=true \
+#    -var node_group_name=blue
+##terraform apply -auto-approve workersblue.plan
+#rm workersblue.plan
+#
+#terraform workspace new "$WORKSPACE-green" || terraform workspace select "$WORKSPACE-green"
+#terraform plan -out workersgreen.plan -input=false \
+#    -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars" \
+#    -var group_enabled=false \
+#    -var node_group_name=green
+##terraform apply -auto-approve workersgreen.plan
+#rm workersgreen.plan
+#popd
+#
+#pushd addons
+#if [ ! -z "$CLEAN" ]; then
+#    rm -rf .terraform
+#fi
+#terraform init -backend-config $WORKSPACESPATH/$WORKSPACE/backend.cfg
+#terraform workspace new "$WORKSPACE-addons" || terraform workspace select "$WORKSPACE-addons"
+#terraform plan -out addons.plan -input=false -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars"
+##terraform apply -auto-approve addons.plan
+#rm addons.plan
+#popd
