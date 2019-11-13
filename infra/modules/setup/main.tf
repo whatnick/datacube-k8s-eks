62dd2a61a5d840b5acd064608e3b779db8ca9684
Santosh Mohan <santosh.mohan@ga.gov.au>
2019-11-13 16:06:46 +1100

    Test

--- infra/modules/setup/main.tf
+++ infra/modules/setup/main.tf
@@ -4,8 +4,8 @@ data "aws_caller_identity" "current" {
 provider "kubernetes" {
   version = "~> 1.9"
   #load_config_file       = false
-  #host                   = var.cluster_endpoint
-  #cluster_ca_certificate = base64decode(var.cluster_ca)
+  host                   = var.cluster_endpoint
+  cluster_ca_certificate = base64decode(var.cluster_ca)
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
     command     = "aws"
